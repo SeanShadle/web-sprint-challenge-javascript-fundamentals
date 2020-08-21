bbce26eddb174580441fb4842b16f549f31905a9
Sean Shadle <seandshadle@gmail.com>
2020-08-21 12:12:33 -0500

    Completed stretch goals on arrays-callback.js and classes.js

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -33,9 +33,12 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map(function(item){
-  return item.animal_name.toLowerCase();
-})
+// const lowCaseAnimalNames = zooAnimals.map(function(item){
+//   return item.animal_name.toLowerCase();
+// })
+// console.log(lowCaseAnimalNames);
+
+const lowCaseAnimalNames = zooAnimals.map((item) => ({name: item.animal_name.toLowerCase()}));
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -43,9 +46,12 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = zooAnimals.filter(function(item){
-  return item.population < 5;
-})
+// const lowPopulationAnimals = zooAnimals.filter(function(item){
+//   return item.population < 5;
+// })
+// console.log(lowPopulationAnimals);
+
+const lowPopulationAnimals = zooAnimals.filter((item) => item.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -53,12 +59,14 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-zooAnimals.reduce(function(accumulator, item){
-  return populationTotal = item.population + accumulator;
-},0)
-console.log(populationTotal);
+// let populationTotal = 0;
+// zooAnimals.reduce(function(accumulator, item){
+//   return populationTotal = item.population + accumulator;
+// },0)
+// console.log(populationTotal);
 
+let populationTotal = zooAnimals.reduce((accumulator, item) => (accumulator += item.population),0);
+console.log(populationTotal);
 
 // ==== Callbacks ====  
 
